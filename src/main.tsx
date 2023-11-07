6c347a2ef125d93d25815312478e681b0e921dae
Yugang Cao <1442770751@qq.com>
2023-11-07 22:10:28 +0800

    chore: workflow config (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 
-ReactDOM.render(
+const root = document.getElementById('root')
+ReactDOM.createRoot(root!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
